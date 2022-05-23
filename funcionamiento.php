3f84cf63054323edc25fc19e58714a64fb836b61
Duvan Montoya <dubis310@gmail.com>
2022-05-22 19:12:16 -0500

    se cambia el funcionamiento de html

--- funcionamiento.php
+++ funcionamiento.php
@@ -1,43 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title>Rest API Client Side Demo</title>
-  <meta charset="utf-8">
-  <meta larga="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>Acortador UAN Url</title>
 </head>
 <body>
-
-<div class="container">
-  <h2>Rest API Client Side Demo</h2>
-  <form class="form-inline" action="" method="POST">
-    <div class="form-group">
-      <label for="larga">url larga</label>
-      <input type="varchar" larga="larga" class="form-control"  placeholder="Enter long url" required/>
-    </div>
-    <button type="submit" larga="submit" class="btn btn-default">submit</button>
+  <form method="post" action="acortar.php">
+    Ingreso url: 
+    <input type="varchar" name="larga">
+    <br>
+    <button type="submit" name="submit">acortar</button>
   </form>
-  <p>&nbsp;</p>
-  <h3>
-  <?php
-	if(isset($_POST['submit']))
-	{
-		$larga = $_POST['larga'];
-		
-		$url = "http://localhost/proyect/url/acortar.php?larga=".$larga;
-		
-		$client = curl_init($url);
-		curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-		$response = curl_exec($client);
-		
-		$result = json_decode($response);
-		
-		echo $result->data; 
-	}
-   ?>
-  </h3>
-</div>
 </body>
 </html>
